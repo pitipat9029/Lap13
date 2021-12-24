ea17d64289821a58c4cbf016c7cc5bab0579279b
Pitipat <pitipat_fu@cmu.ac.th>
2021-12-24 13:43:45 +0700

    commit Lap13_1 fix

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,15 +1,18 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
-int fibonacci(int) ;
+long long int fibonacci(int) ;
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
 
-int fibonacci(int A) {
-    int B ;
+long long int fibonacci(int A) {
+    long long int  B ;
     if (A >= 0) {
         if( A == 0 ) B = 0 ;
         else if ( A == 1) B = 1 ;
