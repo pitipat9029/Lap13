de97a8c4e1f4f63cf04a701b2f570aafbeed1d07
Pitipat <pitipat_fu@cmu.ac.th>
2021-12-24 15:43:17 +0700

    commit lap13_2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
-
+int gcd (int,int) ;
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+int gcd (int A ,int B ) {
+    int C ; 
+    if ( A%B == 0 ) C = B ;
+    else if (A%B != 0){
+        C = gcd( B , A%B ) ;
+    }  
+    return C ;
 }
