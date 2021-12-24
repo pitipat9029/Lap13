75408225891aa30a46eaa977db4127fd6c43d7f7
Pitipat <pitipat_fu@cmu.ac.th>
2021-12-24 13:32:53 +0700

    Commit lap13_1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
-
+int fibonacci(int) ;
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int A) {
+    int B ;
+    if (A >= 0) {
+        if( A == 0 ) B = 0 ;
+        else if ( A == 1) B = 1 ;
+        else B = fibonacci( A - 1 ) + fibonacci( A - 2 ) ;
+    } 
+    return B ;
 }
